78ecca7356db6bb506c33a88515421e7947715b6
Semi-A <nimapopop13@gmail.com>
2026-07-02 04:17:58 +0330

    keep alive

--- web.py
+++ web.py
@@ -1,14 +1,17 @@
 from flask import Flask
+import threading
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Kalemo is alive"
+    return "Kalemo Bot is alive!", 200
 
-def run():
+def run_web():
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080))
+        port=int(os.environ.get("PORT", 10000))
     )
+
+threading.Thread(target=run_web).start()
